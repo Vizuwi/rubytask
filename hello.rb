e82574e46b592a1cea5e99af5079a66d104bf060
Alexey-Zhuk <a.zhuk@sellfree.by>
2020-03-01 16:58:43 +0300

    console puts fix

--- hello.rb
+++ hello.rb
@@ -1,6 +1,5 @@
 require 'nokogiri'
-require 'open-uri'
-require 'json'
+require 'curb'
 require 'csv'
 
 START_PAGE = ARGV.first
@@ -10,20 +9,14 @@ def main
   server_boards = []
   main_page = START_PAGE
   catalog_pages_links(main_page).each do |catalog_page_url|
-    # puts catalog_page_url
-    catalog_page = Nokogiri::HTML(open URI.join(main_page, catalog_page_url))
-    # puts catalog_page
+    catalog_page = Nokogiri::HTML(Curl.get(catalog_page_url).body_str)
     server_boards_links = catalog_page.search("h2.nombre-producto-list a.product-name")
-    # puts server_boards_links
     server_boards_urls = server_boards_links.map { |el| el.attr 'href' }.uniq
     server_boards_names = server_boards_links.map { |el| el.attr 'title' }.uniq
-    # puts "server_boards_urls"
-    # puts server_boards_urls
     server_boards_urls.each_with_index do |server_board_url, index|
       board_name = server_boards_names[index]
       server_boards.concat ( server_board_info(server_board_url, board_name))
     end
-    #puts server_boards
     CSV.open("#{CSV_FILE}", "w") do |csv|
       csv << ["Name", "Price", "Image"]
       server_boards.each do |item|
@@ -36,15 +29,13 @@ end
 
 def catalog_pages_links(main_page)
   pages_of_catalog = [main_page]
-  doc = Nokogiri::HTML(open main_page)
-
-  # because the same buttons on the bottom of main page V
+  doc = Nokogiri::HTML(Curl.get(main_page).body_str)
   link_to_other_pages = doc.xpath("//li[@class='next']/a").map { |el| el.attr 'href' }
   pages_of_catalog += link_to_other_pages.uniq
 end
 
 def server_board_info(url, name)
-  doc = Nokogiri::HTML(open url)
+  doc = Nokogiri::HTML(Curl.get(url).body_str)
   doc.encoding = 'utf-8'
   items = []
   logo = doc.search("a.fancybox img.replace-2x").map { |el| el.attr 'src' }.first
